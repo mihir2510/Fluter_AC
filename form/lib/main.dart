57c8ec91dc1d5f85a5dacb5cf91438c6f8f1bea1
KaustubhDamania <kaustubh.damania@gmail.com>
2019-09-10 03:12:40 +0530

    Added dark background + fixed button rectangle overlay issue

--- form/lib/main.dart
+++ form/lib/main.dart
@@ -22,6 +22,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
+    final double radius = 30.0;
+    final double fontsize = 18.0;
+    final double height = 50;
 
     return new Scaffold(
       appBar: new AppBar(
@@ -31,6 +34,8 @@ class _LoginPageState extends State<LoginPage> {
         //title: new Text('Login'),
         
       ),
+      body: new Scaffold(
+      backgroundColor: Color.fromRGBO(20, 28, 33, 1), //#202833
       body: new Container(
         padding: new EdgeInsets.all(100.0),
         child: new Form(
@@ -38,46 +43,83 @@ class _LoginPageState extends State<LoginPage> {
           child: new ListView(
             children: <Widget>[
               new TextFormField(
+                style: TextStyle(
+                  color: Colors.white,
+                ),
                 keyboardType: TextInputType.emailAddress, // Use email input type for emails.
-                decoration: new InputDecoration(
+                decoration: new InputDecoration(  
                   hintText: 'you@example.com',
-                  labelText: 'E-mail ID'
+                  labelText: 'E-mail ID',
+                  enabledBorder: new UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.white
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.white
+                  )
                 )
               ),
+              SizedBox(height: 10.0),
               new TextFormField(
-                obscureText: true, // Use secure text for passwords.
+                obscureText: true, // Use secure text for passwords.          
+                style: TextStyle(
+                  color: Colors.white,
+                ),
                 decoration: new InputDecoration(
                   hintText: 'Password',
-                  labelText: 'Enter your password'
+                  labelText: 'Password',
+                  enabledBorder: new UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.white
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.white
+                  )
                 )
               ),
               new Container(
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Colors.lightBlue, Colors.lightGreen]
-                  )
+                  
                 ),
                 width: screenSize.width,
-                child: new RaisedButton(
-                  child: new Text(
-                    'Login',
-                    style: new TextStyle(
-                      color: Colors.white
+                child: new InkWell(
+                  onTap: () => print('hello'),
+                  child: new Container(
+                    //width: 100.0,
+                    height: height,
+                    decoration: new BoxDecoration(
+                      gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Colors.lightBlue, Colors.lightGreen]
+                    ),
+                      borderRadius: new BorderRadius.circular(radius),
+                    ),
+                    child: new Center(
+                      child: new Text(
+                        'Login', 
+                        style: new TextStyle(
+                          fontSize: fontsize, 
+                          color: Colors.white
+                        ),
+                      ),
                     ),
                   ),
-                  onPressed: () => null,
-                  color: Colors.transparent,
                 ),
+                  
                 margin: new EdgeInsets.only(
-                  top: 20.0
+                  top: 100.0
                 ),
               )
             ],
           ),
         )
       ),
+    )
     );
   }
 }
